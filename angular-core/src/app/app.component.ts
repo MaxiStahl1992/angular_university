65f213ca8da15766f5982d7edf87655feb21b0fc
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-03-17 15:15:26 +0100

    add initial project

--- angular-core/src/app/app.component.ts
+++ angular-core/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data = {
-    title: 'Angular-Core'
-  }
 
-  onLogoClick() {
-    alert('Hello World!');
-  }
 
-  onKeyUp(newTitle: string) {
-    this.data.title = newTitle;
-  }
+
 }
